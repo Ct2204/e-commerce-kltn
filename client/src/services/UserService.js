c1eb7968256d2360ea152133ac138c99f435d35d
Ct2204 <lecongthuong2204@gmail.com>
2023-12-08 21:07:37 +0700

    update

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -51,6 +51,35 @@ export const register = async (fullName, email, password, confirmPassword) => {
   }
 };
 
+export const logout = async () => {
+  try {
+    const token = store.getState().auth.token;
+      if (!token) {
+        console.error("Token is not available.");
+        return null;
+      }
+    let url = api.url.logout;
+
+    const response = await httpRequest({
+      url: url,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+      
+    });
+    if (response.code === 200) {
+      return response;
+    } else {
+      return response;
+    }
+  } catch (err) {
+    const errMessage = "Error in posting login: ";
+    console.error(errMessage, err);
+    return null;
+  }
+};
+
 
 export const getProfileOfUser = async (id) => {
   try {
