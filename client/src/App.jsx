22813453990cd8018a0c592526203b1ab281b3d2
TranVanThien111 <luonluondandau2001@gmail.com>
2023-12-27 00:14:51 +0700

    UPDATE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
-import DefaultLayout from "./containers/DefaultLayout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import AdminHome from "./pages/AdminHome/AdminHome";
-import AdminLayout from "./containers/AdminLayout";
+import './App.css'
+import DefaultLayout from './containers/DefaultLayout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import AdminHome from './pages/AdminHome/AdminHome'
+import AdminLayout from './containers/AdminLayout'
+import ManageOrder from './pages/ManageOrder/ManageOrder'
 
 function App() {
   return (
@@ -11,10 +12,12 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard/*" element={<AdminLayout />} />
+        <Route path="/dashboard/manageorder/*" element={<ManageOrder />} />
+
         <Route path="/*" element={<DefaultLayout />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
